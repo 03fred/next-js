01faba677255764da054ee5501cfe078d1e9ccd5
frederico.silva <frederico.silva@senior.com.br>
2021-10-19 20:55:14 -0300

    working with double mustache

---
+++ pages/basicos/jsx1.jsx
@@ -0,0 +1,15 @@
+export default function jsx1(){
+
+    const array = [
+        <li>Paulo</li>,
+        <li>Ana</li>,
+        <li>Pedro</li>
+    ];
+
+    const conteudo = (
+                <div>
+                    <h1>JSX #02</h1>
+                </div>
+    )
+    return conteudo;
+}
